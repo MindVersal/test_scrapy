f398ae108adb0a20ae2d50bd70a3b1447350b37e
MindVersal <antajchik@gmail.com>
2017-10-01 20:03:43 +0300

    20:02 01.10.2017

--- tutorial/tutorial/spiders/avito.py
+++ tutorial/tutorial/spiders/avito.py
@@ -10,9 +10,37 @@ class AvitoSpider(scrapy.Spider):
     start_urls = [
         # 'http://www.avito.ru/cherepovets',
         'http://www.avito.ru/cherepovets/dlya_biznesa',
-        'http://www.avito.ru/cherepovets/rabota',
-        'http://www.avito.ru/cherepovets/uslugi',
-        'http://www.avito.ru/cherepovets/zhivotnye',
+        # 'http://www.avito.ru/cherepovets/rabota',
+        # 'http://www.avito.ru/cherepovets/uslugi',
+        # 'http://www.avito.ru/cherepovets/zhivotnye',
+        # 'http://www.avito.ru/cherepovets/hobbi_i_otdyh',
+        # 'http://www.avito.ru/cherepovets/nedvizhimost',
+        # 'http://www.avito.ru/cherepovets/telefony',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/tovary_dlya_kompyutera',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/audio_i_video',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/igry_pristavki_i_programmy',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/fototehnika',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/orgtehnika_i_rashodniki',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/planshety_i_elektronnye_knigi',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/noutbuki',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/nastolnye_kompyutery',  # bytovaya_elektronika
+        # 'http://www.avito.ru/cherepovets/mebel_i_interer',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets/remont_i_stroitelstvo',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets/bytovaya_tehnika',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets/posuda_i_tovary_dlya_kuhni',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets/rasteniya',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets/produkty_pitaniya',  # dlya_doma_i_dachi
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
+        # 'http://www.avito.ru/cherepovets',
         # 'http://www.avito.ru/cherepovets',
     ]
 
